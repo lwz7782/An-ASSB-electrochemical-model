e1fe5f4d4d599d32e0aa7ca83cb3eb7438ab9c50
lwz7782 <93749827+lwz7782@users.noreply.github.com>
2021-11-05 16:06:21 +0800

    Add files via upload

---
+++ assbFoam/assbFoam.C
@@ -0,0 +1,92 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) 2011-2018 OpenFOAM Foundation
+     \\/     M anipulation  | Copyright (C) 2021 Weizhuo Li, Qing Du, and Kui Jiao
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    assbFoam
+
+Description
+    Solves a consists of a set of algorithms, describing the transport of
+    lithium-ion in electrode and SSE, electron conservation, ion conservation,
+    and interface electrochemical reaction.
+    The model computational domain includes negative current collector,
+    negative electrode, solid electrolyte, positive electrode, and positive
+    current collector.
+    
+    This model is made by 
+
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+#include "fvOptions.H"
+#include "simpleControl.H"
+#include "externalWallHeatFluxTemperatureFvPatchScalarField.H"
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+    #include "setRootCaseLists.H"
+
+    #include "createTime.H"
+    #include "createMesh.H"
+
+    simpleControl simple(mesh);
+
+    #include "createFields.H"
+
+    #include "initializeFields.H"
+
+
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    Info<< "\nStarting time loop\n" << endl;
+
+    while (simple.loop(runTime))
+    {
+        Info<< "Time = " << runTime.timeName() << nl << endl;
+
+        #include"calculateFields1.H"
+        #include "C_Li_ion.H"
+        #include "C_Li.H"
+        while (simple.correctNonOrthogonal())
+        {
+            #include "calculateFields2.H"  
+            #include "phi_e.H"
+            #include "phi_ion.H"        
+        }
+
+        runTime.write();  
+ 
+        Info<< "ExecutionTime = " << runTime.elapsedCpuTime() << " s"
+            << "  ClockTime = " << runTime.elapsedClockTime() << " s"
+            << nl << endl;
+
+        #include "stop.H"
+    }
+
+    Info<< "End\n" << endl;
+
+    return 0;
+}
+
+
+// ************************************************************************* //
